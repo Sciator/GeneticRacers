30e1f0a861d8e97a8ba95d81e0ee47d38b2f8850
Roman Wehmhöner <r.wehmhoner@gmail.com>
2019-11-15 17:59:45 +0100

    neural network test

---
+++ src/core/nn/nn.test.ts
@@ -0,0 +1,24 @@
+import { random } from 'mathjs';
+import Jest from 'jest';
+
+import { range } from './../common';
+import { NeuralNet, IANNInitParams } from './nn';
+
+
+describe("neural net", () => {
+  it("should not crash", () => {
+    range(10).slice(1).forEach(x => {
+      range(100).slice(1).forEach(y => {
+        const nnparamsTsst: IANNInitParams = {
+          layerScheme: { input: y, hiddens: [20], output: y, },
+        }
+
+        const nn = NeuralNet.nnCreate(nnparamsTsst);
+        const predict = NeuralNet.predicter(nn);
+        const a = predict(range(y).map(() => random(-1, 1)));
+      })
+    })
+
+  })
+})
+
