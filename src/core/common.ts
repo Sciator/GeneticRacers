d52ba6678a5f7cf4dafd6801db0b4c4983e89193
Roman Wehmhöner <r.wehmhoner@gmail.com>
2019-11-17 17:34:31 +0100

    zip

--- src/core/common.ts
+++ src/core/common.ts
@@ -3,3 +3,7 @@ export const range = (a: number, b?: number) => {
     return [...new Array(a).keys()];
   return [...new Array(b - a).keys()].map(x => x + a);
 };
+
+export function zip<A, B>(arr1: A[], arr2: B[]) {
+  return arr1.map((k, i) => [k, arr2[i]]) as [A, B][];
+}
